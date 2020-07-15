385d7622e3badd62e6bcbec60b2e747b7cc41324
Aris <aristotle.espiritu@moscord.com>
2020-07-15 20:47:41 +0800

    remove global cursor

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <v-app>
     <router-view />
-     <cursor-fx color="#000" outside-size="100" color-hover="#ffffff"/>
   </v-app>
 </template>
-
 <script>
-import { CursorFx } from '@luxdamore/vue-cursor-fx';
-import '@luxdamore/vue-cursor-fx/dist/CursorFx.css';
 export default {
   name: "App",
   methods: {},
-  components: {
-    CursorFx
-  }
 };
 </script>
