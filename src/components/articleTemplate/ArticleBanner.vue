36bd04e93a541842cd2b2458a46e3f6254fa01b5
Aris <aristotle.espiritu@moscord.com>
2020-08-23 20:06:48 +0800

    update author image and padding on articles

--- src/components/articleTemplate/ArticleBanner.vue
+++ src/components/articleTemplate/ArticleBanner.vue
@@ -1,13 +1,22 @@
 <template>
   <v-layout>
     <v-img :src="whiteLogo" class="white-logo"></v-img>
-    <SearchIcon :icon="icon" class="search-icon"/>
-    <v-img data-cursor-hover :src="articleBannerImage" gradient="to top right, rgba(83, 18, 124, 0.44), rgba(201, 117, 255, 0.57)"
-     class="banner">
-     </v-img>
-    <v-row class="banner-desc" no-gutters >
+    <SearchIcon :icon="icon" class="search-icon" />
+    <v-img
+      data-cursor-hover
+      :src="articleBannerImage"
+      gradient="to top right, rgba(83, 18, 124, 0.44), rgba(201, 117, 255, 0.57)"
+      class="banner"
+    >
+    </v-img>
+    <v-row class="banner-desc" no-gutters>
       <v-col cols="12" md="12" lg="6" sm="12">
-        <v-img data-cursor-hover  :src="articleBannerImage" data-cursor-hover class="article-image"></v-img>
+        <v-img
+          data-cursor-hover
+          :src="articleBannerImage"
+          data-cursor-hover
+          class="article-image"
+        ></v-img>
       </v-col>
       <v-col cols="12" md="12" lg="6" sm="12" class="article-desc">
         <div class="artitle-titles">
@@ -16,7 +25,14 @@
           <div class="artitle-author">feat. {{ authorName }}</div>
         </div>
         <div class="author">
-          <v-img data-cursor-hover  :src="authorThumbnail" class="article-thumbnail"></v-img>
+          <v-avatar>
+            <v-img
+              data-cursor-hover
+              :src="authorThumbnail"
+              class="article-thumbnail"
+            ></v-img>
+          </v-avatar>
+
           <div class="author-name">{{ authorName }}</div>
           <div class="date-created">{{ articleDateCreated }}</div>
         </div>
@@ -25,125 +41,131 @@
   </v-layout>
 </template>
 <script>
-import SearchIcon from '@/components/search/searchIcon.vue'
+import SearchIcon from '@/components/search/searchIcon.vue';
 import { CursorFx } from '@luxdamore/vue-cursor-fx';
 import '@luxdamore/vue-cursor-fx/dist/CursorFx.css';
 export default {
-  props: ['pageTitle', 'mainBanner', 'articleBannerImage' , 'articleCategory',
-          'articleTitle', 'authorName', 'authorThumbnail', 'articleDateCreated' ],
+  props: [
+    'pageTitle',
+    'mainBanner',
+    'articleBannerImage',
+    'articleCategory',
+    'articleTitle',
+    'authorName',
+    'authorThumbnail',
+    'articleDateCreated',
+  ],
   name: 'ArticleBanner',
   components: {
     SearchIcon,
-    CursorFx
+    CursorFx,
   },
   data() {
     return {
       whiteLogo: require('@/assets/images/logo-white.svg'),
       icon: require('@/assets/images/search.png'),
-    }
+    };
   },
-  mounted() {
-  },
-  methods: {
-  }
-}
+  mounted() {},
+  methods: {},
+};
 </script>
 <style lang="scss" scope>
 .article-template {
   .search-icon {
-    position:absolute;
-    z-index:5;
-    right:30px;
-    top:30px;
-    cursor:pointer;
+    position: absolute;
+    z-index: 5;
+    right: 30px;
+    top: 30px;
+    cursor: pointer;
   }
   .white-logo {
     position: absolute;
-    z-index:5;
-    left:0;
-    right:0;
-    margin:0 auto;
-    top:20px;
-    width:112px;
+    z-index: 5;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    top: 20px;
+    width: 112px;
   }
 
   .banner-desc {
-    position:absolute;
-    left:0;
-    right:0;
-    margin:0 auto;
-    width:100%;
-    z-index:3;
-    max-height:330px;
-    height:330px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 100%;
+    z-index: 3;
+    max-height: 330px;
+    height: 330px;
     .article-desc {
-      position:relative; 
+      position: relative;
       .artitle-titles {
-        position:absolute;
-        top:30%;
-        left:50px;
-        color:#fff;
-        line-height:40px;
-        .artitle-category{
-          font-size:14px;
-          text-transform:uppercase;
-          margin:0 0 10px;
-          letter-spacing:0.5em;
+        position: absolute;
+        top: 30%;
+        left: 50px;
+        color: #fff;
+        line-height: 40px;
+        .artitle-category {
+          font-size: 14px;
+          text-transform: uppercase;
+          margin: 0 0 10px;
+          letter-spacing: 0.5em;
           font-family: 'Poppins', sans-serif;
-          font-weight:600;
+          font-weight: 600;
         }
         .artitle-title {
-           font-family: 'Libre Baskerville', serif !important;
-          font-size:45px;
-          font-weight:bold;
-          margin:0 0 10px;
+          font-family: 'Libre Baskerville', serif !important;
+          font-size: 45px;
+          font-weight: bold;
+          margin: 0 0 10px;
         }
         .artitle-author {
-          font-size:45px;
-          margin:0 0 10px;
+          font-size: 45px;
+          margin: 0 0 10px;
           font-family: 'Libre Baskerville', serif !important;
         }
       }
     }
     .author {
-      position:absolute;
-      bottom:-70px;
-      color:#000;
-      left:50px;
-      width:100px;
-      text-align:center;
+      position: absolute;
+      bottom: -70px;
+      color: #000;
+      left: 50px;
+      width: 100px;
+      text-align: center;
       .article-thumbnail {
-        width:65px;
-        margin:0 auto;
+        width: 65px;
+        margin: 0 auto;
       }
       .author-name {
-        margin-top:10px;
-        font-size:14px;
-        text-transform:uppercase;
+        margin-top: 10px;
+        font-size: 14px;
+        text-transform: uppercase;
       }
       .date-created {
-        font-size:12px;
+        font-size: 12px;
       }
     }
   }
   .banner {
-    height:322px;
-    max-height:322px;
-    text-align:center;
-    overflow:hidden;
+    height: 322px;
+    max-height: 322px;
+    text-align: center;
+    overflow: hidden;
     .v-image__image {
-       filter: blur(15px);
-       padding: 10px 10px;
-       -webkit-filter: blur(12px);
-       transform: scale(1.2);
+      filter: blur(15px);
+      padding: 10px 10px;
+      -webkit-filter: blur(12px);
+      transform: scale(1.2);
     }
   }
   .article-image {
-    position:absolute;
-    left:5%;
-    width:44%;
-    max-height:450px;
-    z-index:2;
+    position: absolute;
+    left: 5%;
+    width: 44%;
+    max-height: 450px;
+    z-index: 2;
   }
 }
 </style>
--- src/components/readArticles/readArticles.vue
+++ src/components/readArticles/readArticles.vue
@@ -1,108 +1,121 @@
 <template>
   <v-container fluid no-gutters class="pa-0 height100 read-articles">
-  <div class="page-title">{{ title }}</div>
-   <v-row cols="6" md="6" class="">
-     <v-col col="2" md="6" class="article" v-for="article in articles" :key="article.id">
-     <v-list-item  :to="'/' + article.article_type_slug + '/' + article.slug">
-       <v-list-item-content v-scrollanimation>
-        <v-img data-cursor-hover :src="article.image" class="article-images">
-          <div class="article-title">{{ article.category }}</div>
-          <div class="article-label">{{ article.title }}</div>
-        </v-img>
-        </v-list-item-content>
-      </v-list-item>
-     </v-col>
-   </v-row>
+    <div class="page-title">{{ title }}</div>
+    <v-row cols="6" md="6" class="">
+      <v-col
+        col="2"
+        md="6"
+        class="article"
+        v-for="article in articles"
+        :key="article.id"
+      >
+        <v-list-item
+          class="article-items"
+          :to="'/' + article.article_type_slug + '/' + article.slug"
+        >
+          <v-list-item-content v-scrollanimation>
+            <v-img
+              data-cursor-hover
+              :src="article.image"
+              class="article-images"
+            >
+              <div class="article-title">{{ article.category }}</div>
+              <div class="article-label">{{ article.title }}</div>
+            </v-img>
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
 import { CursorFx } from '@luxdamore/vue-cursor-fx';
 import '@luxdamore/vue-cursor-fx/dist/CursorFx.css';
 export default {
-    name: 'ReadArticles',
-    components: {
-      CursorFx
+  name: 'ReadArticles',
+  components: {
+    CursorFx,
+  },
+  data() {
+    return {
+      title: 'Read Articles',
+      articles: [],
+    };
+  },
+  methods: {
+    getArticles() {
+      this.$http.plain
+        .get('articles/mart')
+        .then((response) => {
+          this.articles = response.data;
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
     },
-    data() {
-      return{
-        title: "Read Articles",
-        articles: []
-     }
-   },
-   methods: {
-     getArticles (){
-       this.$http.plain
-         .get('articles/mart')
-         .then((response) => {
-           this.articles = response.data
-         })
-         .catch((error) => {
-           console.log(error.response);
-         });
-     }
-   },
-   mounted() {
-     this.getArticles()
-   }
-}
+  },
+  mounted() {
+    this.getArticles();
+  },
+};
 </script>
 <style lang="scss">
 .read-articles {
-  position:relative;
+  position: relative;
   .page-title {
-    color:#53127C;
-    font-size:42px;
-    margin:20px auto;
-    text-align:center;
+    color: #53127c;
+    font-size: 42px;
+    margin: 20px auto;
+    text-align: center;
     font-family: 'Libre Baskerville', serif !important;
+  }
+  .article-items:before {
+    background: none;
   }
   .article {
     .article-images {
-      transition:.2s;
-      text-align:center;
-      .article-title{
-        max-width:150px;
-        background:#8526DC;
-        text-align:center;
-        color:#fff;
-        font-weight:bold;
-        font-size:18px;
-        position:absolute;
-        top:calc(45% - 16px);
-        left:0;
-        right:0;
-        margin:0 auto;
-        padding:2px 10px;
-
+      transition: 0.2s;
+      text-align: center;
+      .article-title {
+        max-width: 150px;
+        background: #8526dc;
+        text-align: center;
+        color: #fff;
+        font-weight: bold;
+        font-size: 18px;
+        position: absolute;
+        top: calc(45% - 16px);
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        padding: 2px 10px;
       }
       .article-label {
-        font-weight:bold;
-        font-size:42px;
-        color:#fff;
-        position:absolute;
-        top:calc(55% - 16px);
-        left:0;
-        right:0;
-        margin:0 auto;
+        font-weight: bold;
+        font-size: 42px;
+        color: #fff;
+        position: absolute;
+        top: calc(55% - 16px);
+        left: 0;
+        right: 0;
+        margin: 0 auto;
       }
       &:hover {
-        transition:0.2s;
-        transform: translate3d(0,-6px,0);
-        position:relative;
-        opacity:.7;
+        transition: 0.2s;
+        transform: translate3d(0, -6px, 0);
+        position: relative;
+        opacity: 0.7;
       }
     }
-
   }
-
 }
 .before-enter {
-  opacity:0;
+  opacity: 0;
   transform: translateY(100px);
-  transition:all 0.8s ease-out;
+  transition: all 0.8s ease-out;
 }
 .enter {
-  opacity:1;
-  transform:translateY(0px);
+  opacity: 1;
+  transform: translateY(0px);
 }
 </style>
