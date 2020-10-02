64bb8e3b68cb64b2977b5cf77693021f9d5bb235
Aris <aristotle.espiritu@moscord.com>
2020-10-02 22:27:39 +0800

    deploy

---
+++ precache-manifest.08675f8f30943734674a7c6f2adb505f.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990e37548c375f66d248",
+    "url": "/outfittera/css/about.5409e4bc.css"
+  },
+  {
+    "revision": "23f0ab8e32d92bdeb690",
+    "url": "/outfittera/css/chunk-vendors.e6f846c9.css"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/outfittera/fonts/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/outfittera/fonts/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/outfittera/fonts/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/outfittera/fonts/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/outfittera/fonts/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/outfittera/fonts/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/outfittera/fonts/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/outfittera/fonts/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/outfittera/fonts/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/outfittera/fonts/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/outfittera/fonts/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/outfittera/fonts/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "b8da763a858465259ab808951115378a",
+    "url": "/outfittera/img/1.b8da763a.jpg"
+  },
+  {
+    "revision": "ac0814dc8c64b0a865ac8e776596296e",
+    "url": "/outfittera/img/10.ac0814dc.jpg"
+  },
+  {
+    "revision": "0adb8befd0fe4645c8a2246cdcd017a6",
+    "url": "/outfittera/img/11.0adb8bef.jpg"
+  },
+  {
+    "revision": "ea66cb046ef753f891f0f70fa40a8026",
+    "url": "/outfittera/img/12.ea66cb04.jpg"
+  },
+  {
+    "revision": "60faf21f20c66f8dc6ed7ccaf9b509b3",
+    "url": "/outfittera/img/13.60faf21f.jpg"
+  },
+  {
+    "revision": "f9b0a842c06b137dcfc99a9ecc4c0ae4",
+    "url": "/outfittera/img/14.f9b0a842.jpg"
+  },
+  {
+    "revision": "7fdd90b8b829d20e726426a80f7b20da",
+    "url": "/outfittera/img/15.7fdd90b8.jpg"
+  },
+  {
+    "revision": "816a8623f0c68f54c7d8dd660b8df2bd",
+    "url": "/outfittera/img/16.816a8623.jpg"
+  },
+  {
+    "revision": "566c735e6db05bfdacc2eb79695c4f3c",
+    "url": "/outfittera/img/2.566c735e.jpg"
+  },
+  {
+    "revision": "76c4164185efadbdb90a847407760ece",
+    "url": "/outfittera/img/3.76c41641.jpg"
+  },
+  {
+    "revision": "3408054e5f083993c59b1b92204fbc12",
+    "url": "/outfittera/img/4.3408054e.png"
+  },
+  {
+    "revision": "45f3e9b576642cf958503eec6508bff1",
+    "url": "/outfittera/img/6.45f3e9b5.jpg"
+  },
+  {
+    "revision": "fc6c214dbe176f8964f82a9bba7862e5",
+    "url": "/outfittera/img/7.fc6c214d.jpg"
+  },
+  {
+    "revision": "9793a9326317ea5a7ea43faf81c9074f",
+    "url": "/outfittera/img/8.9793a932.jpg"
+  },
+  {
+    "revision": "63046812ea831e0716ab3299564d2493",
+    "url": "/outfittera/img/9.63046812.jpg"
+  },
+  {
+    "revision": "ea06ffb39aec0743c8f49fdc10ad394e",
+    "url": "/outfittera/img/about.ea06ffb3.jpg"
+  },
+  {
+    "revision": "dcd744b5ab6aecb7392c27d3450de007",
+    "url": "/outfittera/img/buy-this-item.dcd744b5.png"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/outfittera/img/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/outfittera/img/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/outfittera/img/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "b8f0ff87aa5c925915bf079de30acf12",
+    "url": "/outfittera/img/fb-circle.b8f0ff87.svg"
+  },
+  {
+    "revision": "9b70bc267b2ed57cd4d393d2ca00699c",
+    "url": "/outfittera/img/fb-gray.9b70bc26.svg"
+  },
+  {
+    "revision": "d7538eb3842bcf610ddca4632775b317",
+    "url": "/outfittera/img/ig-gray.d7538eb3.svg"
+  },
+  {
+    "revision": "67db83bf6fac608547fe8b8fd195b76a",
+    "url": "/outfittera/img/logo-black.67db83bf.svg"
+  },
+  {
+    "revision": "1c13c425c20c76f5a45337938869dcc0",
+    "url": "/outfittera/img/logo-white.1c13c425.svg"
+  },
+  {
+    "revision": "4d115e0b2cad8a42055f60456eb4622f",
+    "url": "/outfittera/img/machine.4d115e0b.jpg"
+  },
+  {
+    "revision": "aa3f72c478a0d6b808a06477f417fd33",
+    "url": "/outfittera/img/makers.aa3f72c4.jpg"
+  },
+  {
+    "revision": "551cf99b10a6245b552f1868c120daf8",
+    "url": "/outfittera/img/miles.551cf99b.jpg"
+  },
+  {
+    "revision": "04a17ec246ad8f401ecc5d0b4ff26dbc",
+    "url": "/outfittera/img/mode.04a17ec2.jpg"
+  },
+  {
+    "revision": "17d8f904fa97d4ce8b383c45912647c1",
+    "url": "/outfittera/img/next-story-img.17d8f904.png"
+  },
+  {
+    "revision": "56e0622510434605a8cbd0a97c0dde8b",
+    "url": "/outfittera/img/no-result.56e06225.png"
+  },
+  {
+    "revision": "19324b1bbeb54fb64f43aa30335a37d8",
+    "url": "/outfittera/img/pinterest-circle.19324b1b.svg"
+  },
+  {
+    "revision": "6a547a1c13469715e753654bcbfc652e",
+    "url": "/outfittera/img/preloader.6a547a1c.gif"
+  },
+  {
+    "revision": "2193ade04a45a7b4b1e1a3494463de55",
+    "url": "/outfittera/img/slideshow2.2193ade0.jpg"
+  },
+  {
+    "revision": "591903b05dd47608ba19a8176002a726",
+    "url": "/outfittera/img/slideshow3.591903b0.jpg"
+  },
+  {
+    "revision": "be4ed2b59a1b2665bfde5809110b644b",
+    "url": "/outfittera/img/slideshow4.be4ed2b5.jpg"
+  },
+  {
+    "revision": "84e4f3d46e072894ff03fc44cde62a90",
+    "url": "/outfittera/img/twitter-circle.84e4f3d4.svg"
+  },
+  {
+    "revision": "f47dfac49c1a0d54ef4fc34426f155a7",
+    "url": "/outfittera/img/video-icon.f47dfac4.png"
+  },
+  {
+    "revision": "0b911e1fe4725e9e1080af511d7cc952",
+    "url": "/outfittera/index.html"
+  },
+  {
+    "revision": "990e37548c375f66d248",
+    "url": "/outfittera/js/about.a3af6731.js"
+  },
+  {
+    "revision": "14acc71e8425987ed044",
+    "url": "/outfittera/js/app.85f86acc.js"
+  },
+  {
+    "revision": "23f0ab8e32d92bdeb690",
+    "url": "/outfittera/js/chunk-vendors.94e033d6.js"
+  },
+  {
+    "revision": "79878ab002e2b2c69b082689f35c850e",
+    "url": "/outfittera/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/outfittera/robots.txt"
+  }
+]);
